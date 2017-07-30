9ad1855537e346e96a7072c617244681716e9131
Fernando Pérez <fperezmaurera@gmail.com>
2017-07-30 20:05:57 +0000

    Añadido el esqueleto de las páginas con un index, una barra de navegación y un logo.

--- project/ac_seguridad/views.py
+++ project/ac_seguridad/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    context = {}
+    template = loader.get_template('ac_seguridad/index.html')
+    return HttpResponse(template.render(context, request))
+    # return HttpResponse("Hello, world. You're at the polls index.")
