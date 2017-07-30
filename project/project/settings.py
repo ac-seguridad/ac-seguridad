f90ca0972604009a6ff0fa92b873dcfdf0cd8b35
Fernando Pérez <fperezmaurera@gmail.com>
2017-07-30 16:57:12 +0000

    configured postgres to work with c9.

--- project/project/settings.py
+++ project/project/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ac_seguridad',
+        'USER': 'ubuntu',
+        'PASSWORD': '2306anayfer',
+        'HOST' : '/var/run/postgresql',
     }
 }
 
