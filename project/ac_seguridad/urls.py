4dff24e34b705d74ff62147d33dc11afcc8834e5
Fernando Pérez <fperezmaurera@gmail.com>
2017-07-31 20:12:50 +0000

    Añadidas una pantalla básica de login y logout.

--- project/ac_seguridad/urls.py
+++ project/ac_seguridad/urls.py
@@ -1,7 +1,13 @@
-from django.conf.urls import url
+# Django Auth.
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
+from django.conf.urls import url
 from . import views
+
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^login/$', auth_views.login, {'template_name': 'ac_seguridad/registration/login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
 ]
