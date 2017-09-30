66c50a83f6404a6747566902d3fd88a7e41c2bb0
Fernando Pérez <fperezmaurera@gmail.com>
2017-09-30 22:47:36 +0000

    se realizo el manejo del mensaje para la entrada

--- project/manejador/cliente.py
+++ project/manejador/cliente.py
@@ -9,32 +9,61 @@
 
 # from ac_seguridad.models import *
 from mysocket import MySocket
+import socket
 
-rif = 'J-1234'
-puerta = 1
-placa = 'AE8MB'
-tipo = 'Salida'
+# Constantes.
+NUM_PUERTA = 1
+RIF = "J-1234"
+
+# Funciones
+def leer_placa():
+    placa = input("Placa: ")
+    return placa
     
 # Programa comienza aquí.
 # ref: https://docs.python.org/3/howto/sockets.html
 # Crear un socket como cliente.
 print("Creando socket")
 socket_cliente = MySocket()
-socket_cliente.connect(host='localhost', port=9999)
+socket_cliente.connect(host='localhost', port=8081)
 print("Socket conectado.")
 
-# Enviar mensaje:
+# Enviar primer mensaje:
+# Estructura del primer mensaje:
+# * RIF: lleno
+# * ticket: None.
+# * placa: llena.
+# * accion: entrada.
+# * tipo: llena ('placa_leida')
+# * puerta: llena.
 print("Preparando mensaje")
 mensaje = dict()
-mensaje['estacionamiento'] = rif
-mensaje['placa'] = placa
-mensaje['puerta'] = puerta
-mensaje['tipo'] = tipo
+mensaje['estacionamiento'] = RIF
+mensaje['ticket'] = None
+mensaje['placa'] = leer_placa()
+mensaje['puerta'] = NUM_PUERTA
+mensaje['accion'] = 'entrada'
+mensaje['tipo'] = 'placa_leida'
 
 print("Enviando mensaje: {}".format(mensaje))
 socket_cliente.sendall_json(mensaje)
 # socket_cliente.mysend("Hola, este es el mensaje\0".encode(encoding="utf-8", errors="strict"))
 print("Mensaje enviado")
+
+print("Recibiendo respuesta")
+respuesta = socket_cliente.receive()
+print("Respuesta recibida: {}".format(respuesta))
+
+if (respuesta['tipo'] == "OK_entrada_estacionamiento"):
+    print("Luz verde.")
+    
+elif (respuesta['tipo'] == "NO_entrada_estacionamiento"):
+    print("Luz roja.")
+else:
+    print("Respuesta no válida")
+
+socket_cliente.sock.shutdown(socket.SHUT_WR)
+socket_cliente.sock.close()
 
 
 
--- project/manejador/servers.py
+++ project/manejador/servers.py
@@ -1,15 +1,124 @@
 import socketserver
-
 import socket
 import threading
 import os
-import socketserver
 import json
-import mysocket
+import sys
+
+# Para cargar django ref1: https://stackoverflow.com/questions/8047204/django-script-to-access-model-objects-without-using-manage-py-shell
+# Respuesta de: Alkindus
+# ref2: https://stackoverflow.com/questions/41518910/how-to-make-a-script-to-insert-data-in-my-default-sqlite3-database-django
+# Respuesta de: skoll
+import django
+sys.path.append('/home/ubuntu/workspace/project')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'project.settings'
+django.setup()
+
+from ac_seguridad.models import *
+import manejador.mysocket as mysocket
+from django.utils import timezone
 
 class MalformedMessage(Exception): pass
 class ConnectionClosed(Exception): pass
 
+def manejar_mensaje(mensaje):
+    '''
+        args:
+            mensaje: es un diccionario que contiene las claves definidas en keys.
+        keys:
+            estacionamiento: es el rif del estacionamiento, con formato J-XXXXXXXX.
+            placa: refiere a la placa del vehículo con formato venezolano.
+            puerta: refiere a la puerta de entrada del estacionamiento.
+            tipo: tipo de mensaje y sus valores están dentro de tipos.
+            ticket: 
+            accion: entrada o salida.
+        tipos:
+            ambos:
+                placa_leida.
+            entrada:
+                entrada_estacionamiento,
+            salida:
+                salida_estacionamiento
+            
+            ticket_pagado,
+            
+            
+        returns: devuelve al cliente un mensaje diciendo 'OK_entrada' o 'NO_entrada'
+        
+            
+    '''
+    tipo = mensaje['tipo']
+    rif = mensaje['estacionamiento']
+    placa = mensaje['placa']
+    puerta = mensaje['puerta']
+    accion = mensaje['accion']
+    
+    respuesta = mensaje.copy()
+    vehiculo = None
+    permite_entrada = False
+    estacionamiento = Estacionamiento.objects.get(rif=rif)
+    if (tipo == 'placa_leida'):
+        # Como nos aseguramos que SIEMPRE vamos a recibir una placa válida,
+        # no tenemos que hacer esas verificaciones.
+        try:
+            vehiculo = Vehiculo.objects.get(placa=placa)
+        
+        except Vehiculo.DoesNotExist:
+            # TODO: ver si el estacionamiento permite entrada o no.
+            print("el Vehiculo {} no existe en la base de datos".format(placa))
+            
+        #vehiculo registrado
+        if (vehiculo is not None):
+            respuesta['tipo'] = "OK_entrada_estacionamiento"
+            ticket = generar_ticket_registrados(vehiculo,estacionamiento)
+            respuesta['ticket'] = ticket.numero_ticket
+            generar_actividad(estacionamiento=estacionamiento,
+                              ticket=ticket,
+                              vehiculo=vehiculo,
+                              persona = vehiculo.dueno,
+                              tipo="entrada_estacionamiento")
+            
+        #genera un ticket de los no registrados pero si hay           
+        if ((vehiculo is None) and (not estacionamiento.acceso_restringido)):
+            respuesta['tipo'] = "OK_entrada_estacionamiento"
+            respuesta['ticket'] = generar_ticket_no_registrados(placa,estacionamiento)
+            
+        #No permite entrada por no estar registrado y tener acceso rest.
+        if ((vehiculo is None) and (estacionamiento.acceso_restringido)):
+            respuesta['tipo'] = "NO_entrada_estacionamiento"
+            respuesta['ticket'] = None
+            
+    return respuesta
+
+def generar_ticket_registrados(vehiculo,estacionamiento): 
+    ticket = Ticket(placa =  vehiculo,
+                    rif = estacionamiento,
+                    hora_entrada =  timezone.now(),
+                    hora_salida =  None,
+                    pagado = False 
+                   )
+    ticket.save()
+    return ticket
+
+def generar_ticket_no_registrados(placa,estacionamiento): 
+    ticket = TicketNoRegistrado(placa =  placa,
+                    rif = estacionamiento,
+                    hora_entrada =  timezone.now(),
+                    hora_salida =  None,
+                    pagado = False 
+                   )
+    ticket.save()
+    return ticket.numero_ticket
+    
+def generar_actividad(estacionamiento, vehiculo, persona, tipo, ticket=None):
+    actividad = Actividad(estacionamiento=estacionamiento, 
+                          vehiculo=vehiculo,
+                          usuario=persona,
+                          tipo=tipo,
+                          ticket=ticket,
+                          fecha=timezone.now())
+    actividad.save()
+    
 ##############################################################################
 ############################## TCP y FILES ###################################
 ##############################################################################
@@ -39,17 +148,16 @@ class FilesTCPHandler(socketserver.StreamRequestHandler):
 class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
-        # data = str(self.request.recv(1024), 'ascii')
-        # cur_thread = threading.current_thread()
-        # response = bytes("{}: {}".format(cur_thread.name, data), 'ascii')
-        # self.request.sendall(response)
-        
         sock = mysocket.MySocket(self.request)
         mensaje = sock.receive()
+        respuesta = manejar_mensaje(mensaje)
+        sock.sendall_json(respuesta)
         print("Mensaje recibido: {}".format(mensaje))
-        cur_thread = threading.current_thread()
-        response = bytes("{}: {}".format(cur_thread.name, mensaje), 'ascii')
-        self.request.sendall(response)
+        print("Mensaje enviado: {}".format(respuesta))
+        
+        # cur_thread = threading.current_thread()
+        # response = bytes("{}: {}".format(cur_thread.name, respuesta), 'ascii')
+        # self.request.sendall(response)
         
 class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     pass
@@ -62,39 +170,12 @@ class ForkedTCPRequestHandler(socketserver.BaseRequestHandler):
     def handle(self):
         sock = mysocket.MySocket(self.request)
         mensaje = sock.receive()
+        respuesta = manejar_mensaje(mensaje)
+        sock.sendall_json(respuesta)
         print("Mensaje recibido: {}".format(mensaje))
-        response = bytes("{}: {}".format(os.getpid(), mensaje), 'ascii')
-        self.request.sendall(response)
+        print("Mensaje enviado: {}".format(respuesta))
+        # response = bytes("{}: {}".format(os.getpid(), mensaje), 'ascii')
+        # self.request.sendall(response)
 
 class ForkedTCPServer(socketserver.ForkingMixIn, socketserver.TCPServer):
     pass
-
-
-def client(ip, port, message):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.connect((ip, port))
-        sock.sendall(bytes(message, 'ascii'))
-        response = str(sock.recv(1024), 'ascii')
-        print("Received: {}".format(response))
-
-if __name__ == "__main__":
-    # Port 0 means to select an arbitrary unused port
-    HOST, PORT = "localhost", 0
-
-    server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
-    with server:
-        ip, port = server.server_address
-
-        # Start a thread with the server -- that thread will then start one
-        # more thread for each request
-        server_thread = threading.Thread(target=server.serve_forever)
-        # Exit the server thread when the main thread terminates
-        server_thread.daemon = True
-        server_thread.start()
-        print("Server loop running in thread:", server_thread.name)
-
-        client(ip, port, "Hello World 1")
-        client(ip, port, "Hello World 2")
-        client(ip, port, "Hello World 3")
-
-        server.shutdown()
