c78dcea7cfac5550975125258ca097b94ef1efd7
Fernando Benjamín Pérez Maurera <fperezmaurera@gmail.com>
2017-12-02 20:44:55 -0400

    Se añadió la IP de la casa como host aceptado.

--- project/project/settings.py
+++ project/project/settings.py
@@ -26,7 +26,7 @@ with open('/etc/secret_key_acseguridad.txt') as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','190.38.144.154']
+ALLOWED_HOSTS = ['localhost','190.38.144.154', '190.39.1.134']
 
 
 # Application definition
